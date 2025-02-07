2fa9e8d64c185aa4e8a77b4f675bcb6b274b8298
chimegerdene <chimgeebatuh@gmail.com>
2025-02-07 12:25:45 +0800

    shjasd

--- index.js
+++ index.js
@@ -56,56 +56,9 @@ function createElement(elementName, appendElement, className, innerText) {
     ) {
       display.value += " " + innerText + " ";
     } else if (innerText === "=") {
-      eq = display.value.split(" ");
-      // eq = eval(display.value);
-      // switch (eq[1]) {
-      //   case "+":
-      //     display.value = Number(eq[0]) + Number(eq[2]);
-      //     break;
-      //   case "-":
-      //     display.value = Number(eq[0]) - Number(eq[2]);
-      //     break;
-      //   case "*":
-      //     display.value = (Number(eq[0]) * Number(eq[2])).toFixed(2);
-      //     break;
-      //   case "/":
-      //     display.value = (Number(eq[0]) / Number(eq[2])).toFixed(2);
-      //     break;
-      //   case "%":
-      //     display.value = (Number(eq[0]) * (Number(eq[2]) / 100)).toFixed(2);
-      //     break;
-      // }
-      // display.value = eq;
-      console.log(eq);
-      let st = [];
-      let num1 = 0;
-      let num2 = 0;
-
-      for (let i = 0; i < eq.length; i++) {
-        if (eq[i] === "-") {
-          // display.value = (Number(eq[i - 1]) - Number(eq[i + 1])).toFixed(2);
-          num2 = eq[i + 1];
-          num1 = st.pop();
-          display.value = num1 - num2;
-          st.push(display.value);
-          console.log(num1, num2);
-        } else if (eq[i] === "+") {
-          display.value = (Number(eq[i - 1]) + Number(eq[i + 1])).toFixed(2);
-        } else if (eq[i] === "*") {
-          display.value = (Number(eq[i - 1]) * Number(eq[i + 1])).toFixed(2);
-        } else if (eq[i] === "/") {
-          display.value = (Number(eq[i - 1]) / Number(eq[i + 1])).toFixed(2);
-        } else if (eq[i] === "%") {
-          display.value = (
-            (Number(eq[i - 1]) * Number(eq[i + 1])) /
-            100
-          ).toFixed(2);
-        } else {
-          st.push(eq[i]);
-        }
-      }
+      display.value = result(display.value);
     } else if (innerText === "AC") {
-      display.value = null;
+      display.value = "";
     } else {
       display.value += innerText;
     }
@@ -115,4 +68,107 @@ function createElement(elementName, appendElement, className, innerText) {
 
 for (let i = 0; i < array.length; i++) {
   createElement("button", btnCont, "btn", array[i]);
+}
+
+function result(equation) {
+  let stack = [];
+  let operations = [];
+  let temp = equation.split(" ");
+
+  console.log("temp: ", temp);
+  for (let i = 0; i < temp.length; i++) {
+    if (
+      temp[i] === "-" ||
+      temp[i] === "+" ||
+      temp[i] === "*" ||
+      temp[i] === "/" ||
+      temp[i] === "%"
+    ) {
+      operations.push(temp[i]);
+    } else {
+      stack.push(temp[i]);
+    }
+  }
+  console.log("op:", operations);
+  console.log("number:", stack);
+
+  let num1, num2;
+  const m = operations.length;
+  let res = 0;
+
+  let f;
+
+  if (operations.includes("*")) {
+    f = operations.indexOf("*");
+    let mult = stack[f] * stack[f + 1];
+    stack[f] = mult;
+
+    stack.splice(f, 2, mult);
+    operations.splice(f, 1);
+  } else if (operations.includes("/")) {
+    f = operations.indexOf("/");
+    let mult = (stack[f] / stack[f + 1]).toFixed(2);
+    stack[f] = mult;
+
+    stack.splice(f, 2, mult);
+    operations.splice(f, 1);
+  }
+
+  console.log("* op:", operations);
+  console.log("* number:", stack);
+
+  stackReversed = stack.reverse();
+
+  operationsReversed = operations.reverse();
+
+  console.log("reversed op:", operationsReversed);
+  console.log("reversed number:", stackReversed);
+
+  //calculating
+  while (operationsReversed.length) {
+    let op = operationsReversed.pop();
+    num2 = stackReversed.pop();
+    num1 = stackReversed.pop();
+
+    switch (op) {
+      case "+":
+        res = Number(num2) + Number(num1);
+        console.log(res);
+        stackReversed.push(res);
+        console.log(stackReversed);
+        break;
+      case "-":
+        // if (num2 < 0) {
+        //   res = Number(num2) - Number(num1);
+        // } else {
+        //   res = Number(num1) - Number(num2);
+        // }
+        res = Number(num2) - Number(num1);
+        console.log(res);
+        stackReversed.push(res);
+        console.log("pushed", stackReversed);
+        break;
+      // case "*":
+      //   res = (Number(num2) * Number(num1)).toFixed(2);
+      //   console.log(res);
+      //   stackReversed.push(res);
+      //   console.log(stackReversed);
+      //   break;
+      // case "/":
+      //   res = (Number(num2) / Number(num1)).toFixed(2);
+      //   console.log(res);
+      //   stackReversed.push(res);
+      //   console.log(stackReversed);
+      //   break;
+      case "%":
+        res = ((Number(num1) * Number(num2)) / 100).toFixed(2);
+        console.log(res);
+        stackReversed.push(res);
+        console.log(stackReversed);
+        break;
+    }
+  }
+  console.log(stackReversed, "returned value");
+
+  return stackReversed.pop();
 }
